724ce060fc4eeeca49ae0619abcaf8a486a47031
DS-expert <workmy.chd@gmail.com>
2025-11-06 19:34:44 +0500

    chore(config): Add Train and Test features Path to configuration

--- config/config.py
+++ config/config.py
@@ -21,6 +21,12 @@ RAW_DATA_PATH = RAW_DATA_DIR / "fake_job_postings.csv"
 PROCESSED_DATA_DIR = DATA_DIR / "Preprocessed"
 PROCESSED_DATA_PATH = PROCESSED_DATA_DIR / "preprocessed_data.csv"
 
+TRAIN_FEATURES_PATH = PROCESSED_DATA_DIR / "train_features.npz"
+TEST_FEATURES_PATH = PROCESSED_DATA_DIR / "test_features.npz"
+
+TRAIN_TARGET_PATH = PROCESSED_DATA_DIR / "train_target.npy"
+TEST_TARGET_PATH = PROCESSED_DATA_DIR / "test_target.npy"
+
 # Train Hyperparameter
 
 RANDOM_STATE = 42
