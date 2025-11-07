374c1f5f29ac0fa8497d11ca2e186a55fc7083c6
DS-expert <workmy.chd@gmail.com>
2025-11-07 21:29:08 +0500

    feat(evaluate): Wrote code of evaluating the trained model and return evaluation metrics

---
+++ src/evaluation.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+from train import train_adaboost
+from sklearn.metrics import accuracy_score, roc_auc_score, classification_report
+
+def evaluate_mode(model, X_test, y_test):
+    """
+    Evaluate the given model on the test dataset and return accuracy, ROC AUC Score and classification report.
+
+    Parameters:
+    model: Trained machine learning model
+    X_test: Feature of the test dataset
+    y_test: True labels of the test dataset
+
+    *returns: accuracy_score, roc_auc_score, classification_report
+    """
+    y_pred = model.predict(X_test)
+
+    acc_score = accuracy_score(y_test, y_pred)
+    roc = roc_auc_score(y_test, y_pred)
+    cr = classification_report(y_test, y_pred)
+
+    return {
+        "Accuracy Score": acc_score,
+        "ROC AUC Score ": roc,
+        "Classification Report" : cr
+    }
